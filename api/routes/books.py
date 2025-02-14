a6fdae590a055a5411f7352ca60e2e20dcf9adc8
Abdulbasit Abdulrasheed <email>
2025-02-14 11:42:47 +0100

    testing CD pipeline

--- api/routes/books.py
+++ api/routes/books.py
@@ -57,7 +57,7 @@ async def get_book(book_id: int) -> Book:
     if not book:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="Book not found"
+            detail="Book not found!"
         )
     return book
 
